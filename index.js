2f51795f1c5f9ade5ff277b3f1da7eb7d4d10a62
julie-mac <123594716+julie-mac@users.noreply.github.com>
2023-08-02 21:32:46 -0400

    input questions to ask user inside index.js

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
-
+const {Square, Circle, Triangle} = require("./shapes")
+const createLogo = require('./lib/')
 
 const questions = [
     {
         type: "input",
         name: "fontColour",
         message: "Please enter the colour (or hex code!) you would like your font to be."
+    },
+    {
+        type: "input",
+        name: "shapeColour",
+        message: "Please enter the colour (or hex code!) you would like your shape to be."
+    },
+    {
+        type: "list",
+        name: "shape",
+        message: "What shape would you like your logo to be?",
+        choices: ["Square", "Circle", "Triangle"]
+    },
+    {
+        type: "input",
+        name: "name",
+        message: "Please enter up to three characters to put inside your logo."
     }
 ]
+
+function init() {
+    inquirer.prompt(questions)
+    .then((data) => {
+        writeToFile('logo.svg', (data.name, data.shape, data.shapeColour, data.fontColour) => {
+            
+        })
+    })
+}
